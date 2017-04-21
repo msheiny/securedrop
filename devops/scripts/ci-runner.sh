239ebda2c8b55926072d0bccc4006099a88be910
Michael Sheinberg <mike@freedom.press>
2017-04-21 11:16:39 -0400

    Ensure debian packages are built in CI
    Previously had the skip local_build tag on for testing purposes.

--- devops/scripts/ci-runner.sh
+++ devops/scripts/ci-runner.sh
@@ -4,4 +4,4 @@ export RETRY_FILES_ENABLED="False"
 export ANSIBLE_INVENTORY="./devops/inventory/$CI_SD_ENV"
 export ANSIBLE_SSH_ARGS="-F $HOME/.ssh/sshconfig-securedrop-ci-$TRAVIS_BUILD_NUMBER -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-ansible-playbook install_files/ansible-base/build-deb-pkgs.yml --skip-tags=local_build
+ansible-playbook install_files/ansible-base/securedrop-${CI_SD_ENV}.yml --skip-tags="grsec" -e primary_network_iface=eth0
