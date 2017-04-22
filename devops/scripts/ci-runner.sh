6043e8c773b1bba3622fa8e6cb33745b80de4655
Michael Sheinberg <mike@freedom.press>
2017-04-22 16:00:26 -0400

    fix env -> build number, skip vagrant deb build

--- devops/scripts/ci-runner.sh
+++ devops/scripts/ci-runner.sh
@@ -2,6 +2,6 @@
 
 export RETRY_FILES_ENABLED="False"
 export ANSIBLE_INVENTORY="./devops/inventory/$CI_SD_ENV"
-export ANSIBLE_SSH_ARGS="-F $HOME/.ssh/sshconfig-securedrop-ci-$TRAVIS_BUILD_NUMBER -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+export ANSIBLE_SSH_ARGS="-F $HOME/.ssh/sshconfig-securedrop-ci-$BUILD_NUM -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-ansible-playbook install_files/ansible-base/securedrop-${CI_SD_ENV}.yml --skip-tags="grsec" -e primary_network_iface=eth0
+ansible-playbook install_files/ansible-base/securedrop-${CI_SD_ENV}.yml --skip-tags="grsec,local_build" -e primary_network_iface=eth0
