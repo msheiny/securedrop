7f09bbabaa9f42bb3815a59ac1d1207a8c1bea60
Michael Sheinberg <mike@freedom.press>
2017-04-24 12:48:17 -0400

    Optionally skip CI tear-down
    This is useful when live debugging CI environment with job
    running in background.

--- devops/scripts/ci-teardown.sh
+++ devops/scripts/ci-teardown.sh
@@ -6,4 +6,6 @@
 export ANSIBLE_INVENTORY="localhost"
 export RETRY_FILES_ENABLED=False
 
-./devops/playbooks/aws-ci-teardown.yml --diff
+if [ ! -f "$HOME/.FPF_CI_DEBUG" ]; then
+    ./devops/playbooks/aws-ci-teardown.yml --diff
+fi
