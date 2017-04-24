91047ef91bfb7600cf178fabdfe6c81a16006a48
Michael Sheinberg <mike@freedom.press>
2017-04-23 21:10:23 -0400

    Ensure submodules are being checked out in CI

--- devops/scripts/ci-runner.sh
+++ devops/scripts/ci-runner.sh
@@ -4,6 +4,10 @@ export RETRY_FILES_ENABLED="False"
 export ANSIBLE_INVENTORY="./devops/inventory/$CI_SD_ENV"
 export ANSIBLE_SSH_ARGS="-F $HOME/.ssh/sshconfig-securedrop-ci-$BUILD_NUM -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
+if [ ! -d "install_files/ossec-packages/ansible/" ]; then
+    git submodule update --init --recursive
+fi
+
 # Build OSSEC agent+server packages
 ansible-playbook install_files/ossec-packages/ansible/build-deb-pkgs.yml -e build_path=/tmp/ossec-build -e repo_src_path="/tmp/ossec-src" -e local_build_path="../../../build/"
 
