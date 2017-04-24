795ea4c820225768fa8b59c404a019b025e07c1b
Michael Sheinberg <mike@freedom.press>
2017-04-23 20:38:28 -0400

    Submodule and build OSSEC packages for remote CI
    Needed to make some upstream tweaks to the ossec repo and pass in some
    additional variable over-rides. The result is at the end of the build process,
    all the deb packages get dumped into `./build` and sent over to mon + app
    in a later playbook.

--- devops/scripts/ci-runner.sh
+++ devops/scripts/ci-runner.sh
@@ -4,4 +4,8 @@ export RETRY_FILES_ENABLED="False"
 export ANSIBLE_INVENTORY="./devops/inventory/$CI_SD_ENV"
 export ANSIBLE_SSH_ARGS="-F $HOME/.ssh/sshconfig-securedrop-ci-$BUILD_NUM -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
+# Build OSSEC agent+server packages
+ansible-playbook install_files/ossec-packages/ansible/build-deb-pkgs.yml -e build_path=/tmp/ossec-build -e repo_src_path="/tmp/ossec-src" -e local_build_path="../../../build/"
+
+# Build + install OSSEC config packages, install securedrop
 ansible-playbook install_files/ansible-base/securedrop-${CI_SD_ENV}.yml --skip-tags="grsec,local_build" -e primary_network_iface=eth0 -e install_local_packages=true
