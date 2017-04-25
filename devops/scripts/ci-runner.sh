1482afb71031736b893907b2bb08ea9c7c6070b8
Michael Sheinberg <mike@freedom.press>
2017-04-25 11:33:31 -0400

    Move testing injection logic to test script
    Additionally ensure we are cleaning up previous test results on each run.
    If it sees an existing test file the job will bomb out.

--- devops/scripts/ci-runner.sh
+++ devops/scripts/ci-runner.sh
@@ -12,13 +12,6 @@ ansible staging -b -m hostname -a "name={{inventory_hostname}}"
 # Hotfix for missing hostname attached to localhost
 ansible staging -b -m lineinfile -a "dest=/etc/hosts line='127.0.0.1 localhost {{inventory_hostname}}' regexp='^127\.0\.0\.1'"
 
-# Quick hack to pass IPs off to testinfra
-for host in app mon; do
-    # tacking xargs at the end strips the trailing space
-    ip=`ssh -F $HOME/.ssh/sshconfig-securedrop-ci-$BUILD_NUM ${host}-$CI_SD_ENV "hostname -I" | xargs`
-    ansible -c local localhost -m lineinfile -a "dest=./testinfra/vars/app-${CI_SD_ENV}.yml line='${host}_ip: $ip' regexp='^${host}_ip'"
-    ansible -c local localhost -m lineinfile -a "dest=./testinfra/vars/mon-${CI_SD_ENV}.yml line='${host}_ip: $ip' regexp='^${host}_ip'"
-done
 
 # Build OSSEC agent+server packages
 ansible-playbook install_files/ossec-packages/ansible/build-deb-pkgs.yml -e build_path=/tmp/ossec-build -e repo_src_path="/tmp/ossec-src" -e local_build_path="../../../build/"
--- devops/scripts/spin-run-test.sh
+++ devops/scripts/spin-run-test.sh
@@ -4,6 +4,14 @@
 #
 . devops/ansible_env
 
+# Quick hack to pass IPs off to testinfra
+for host in app mon; do
+    # tacking xargs at the end strips the trailing space
+    ip=`ssh -F $HOME/.ssh/sshconfig-securedrop-ci-$BUILD_NUM ${host}-$CI_SD_ENV "hostname -I" | xargs`
+    ansible -c local localhost -m lineinfile -a "dest=./testinfra/vars/app-${CI_SD_ENV}.yml line='${host}_ip: $ip' regexp='^${host}_ip'"
+    ansible -c local localhost -m lineinfile -a "dest=./testinfra/vars/mon-${CI_SD_ENV}.yml line='${host}_ip: $ip' regexp='^${host}_ip'"
+done
+
 # Only run test task (usually used in local testing)
 if [ ! "$1" == "only_test" ]; then
     make ci-spinup && make ci-run
@@ -24,5 +32,6 @@ fi
 if [ -z "$TEST_REPORTS" ]; then
     export TEST_REPORTS=`pwd`
 fi
+rm -r $TEST_REPORTS/junit || true
 mkdir $TEST_REPORTS/junit || true
 ./testinfra/combine-junit.py *results.xml > $TEST_REPORTS/junit/junit.xml
