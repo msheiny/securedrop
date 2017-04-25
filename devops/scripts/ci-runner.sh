a43e5b465402b7279ce28e07522d3bee01710379
Michael Sheinberg <mike@freedom.press>
2017-04-25 10:00:25 -0400

    Support to spin-up staging environment in AWS

---
+++ devops/scripts/ci-runner.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export RETRY_FILES_ENABLED="False"
+export ANSIBLE_INVENTORY="./devops/inventory/$CI_SD_ENV"
+export ANSIBLE_SSH_ARGS="-F $HOME/.ssh/sshconfig-securedrop-ci-$BUILD_NUM -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+if [ ! -d "install_files/ossec-packages/ansible/" ]; then
+    git submodule update --init --recursive
+fi
+
+# Set hostnames for CI boxes based on inventory names
+ansible staging -b -m hostname -a "name={{inventory_hostname}}"
+
+# Hotfix for missing hostname attached to localhost
+ansible staging -b -m lineinfile -a "dest=/etc/hosts line='127.0.0.1 localhost {{inventory_hostname}}' regexp='^127\.0\.0\.1'"
+
+# Quick hack to pass IPs off to testinfra
+for host in app mon; do
+    # tacking xargs at the end strips the trailing space
+    ip=`ssh -F $HOME/.ssh/sshconfig-securedrop-ci-$BUILD_NUM ${host}-$CI_SD_ENV "hostname -I" | xargs`
+    ansible -c local localhost -m lineinfile -a "dest=./testinfra/vars/app-${CI_SD_ENV}.yml line='${host}_ip: $ip' regexp='^${host}_ip'"
+    ansible -c local localhost -m lineinfile -a "dest=./testinfra/vars/mon-${CI_SD_ENV}.yml line='${host}_ip: $ip' regexp='^${host}_ip'"
+done
+
+# Build OSSEC agent+server packages
+ansible-playbook install_files/ossec-packages/ansible/build-deb-pkgs.yml -e build_path=/tmp/ossec-build -e repo_src_path="/tmp/ossec-src" -e local_build_path="../../../build/"
+
+# Build + install OSSEC config packages, install securedrop
+ansible-playbook install_files/ansible-base/securedrop-${CI_SD_ENV}.yml --skip-tags="grsec,local_build" -e primary_network_iface=eth0 -e install_local_packages=true -e ssh_users="$USER"
+
+# Reboot and wait
+./devops/playbooks/reboot_and_wait.yml --diff
---
+++ devops/scripts/spin-run-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#
+#
+
+# Only run test task (usually used in local testing)
+if [ ! "$1" == "only_test" ]; then
+    make ci-spinup && make ci-run
+fi
+
+if [ "$?" == "0" ]; then
+    case "$CI_SD_ENV" in
+    "staging")
+        ./testinfra/test.py app-$CI_SD_ENV
+        ./testinfra/test.py mon-$CI_SD_ENV
+        ;;
+    "development")
+        ./testinfra/test.py development
+        ;;
+    esac
+fi
+
+if [ -z "$TEST_REPORTS" ]; then
+    export TEST_REPORTS=`pwd`
+fi
+mkdir $TEST_REPORTS/junit || true
+./testinfra/combine-junit.py *results.xml > $TEST_REPORTS/junit/junit.xml
